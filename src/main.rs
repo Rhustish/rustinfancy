fc846e21c29a11214a27254c42873f9b3f17cde8
Bhavy Bhatnagar <bbhatnagar27@gmail.com>
2024-06-01 18:43:02 +0530

    hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
-fn main() {
-    println!("Hello, world!");
+use crossterm::{
+    event::{self,KeyCode,KeyEventKind}, execute, terminal::{
+        disable_raw_mode, enable_raw_mode, EnterAlternateScreen,LeaveAlternateScreen
+    }, ExecutableCommand
+};
+
+use ratatui::{
+    prelude::{CrosstermBackend,Stylize,Terminal},
+    widgets::Paragraph,
+};
+
+use::std::io::{stdout, Result};
+
+fn main()-> Result<()> {
+    stdout().execute(EnterAlternateScreen)?;
+    enable_raw_mode()?;
+    let mut term = Terminal::new(CrosstermBackend::new(stdout()))?;
+    term.clear()?;
+
+    //main loop
+    loop{
+        //draw the ui
+        term.draw(|frame| {
+            let area = frame.size();
+            frame.render_widget(Paragraph::new("Hello World!").white().on_blue(), area);
+        })?;
+        //handle events
+        if event::poll(std::time::Duration::from_millis(16))?{
+            if let event::Event::Key(key) = event::read()? {
+                if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q') { break; }
+            } 
+        }
+    }
+
+
+    stdout().execute(LeaveAlternateScreen)?;
+    disable_raw_mode()?;
+    Ok(())
 }
+
+// fn main() {
+//     std::process::exit(real_main());
+// }
+
+
